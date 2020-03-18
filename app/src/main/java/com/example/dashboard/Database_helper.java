a47929a5ed884410bd6760bb25fb926463c1b5a3
kishnr <kishrmsh@gmail.com>
2020-03-18 10:31:15 +0530

    Menu and Login

---
+++ app/src/main/java/com/example/dashboard/Database_helper.java
@@ -0,0 +1,19 @@
+/*package com.example.dashboard;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+public class Database_helper extends SQLiteOpenHelper {
+
+    public Database_helper(Context context){
+        super(context, )
+    }
+
+    public void onCreate(SQLiteDatabase db){
+    }
+
+}
+*/
+
+
--- app/src/main/java/com/example/dashboard/MainActivity.java
+++ app/src/main/java/com/example/dashboard/MainActivity.java
@@ -2,16 +2,48 @@ package com.example.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText password;
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        submit = (Button)findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(), password.getText().toString());
+            }
+        });
     }
 
-    public void sendMessage(View view){}
+    public void validate(String name, String password){
+        if(name.equals("User")&& password.equals("1234")) {
+            Intent intent = new Intent(MainActivity.this, Menu.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Incorrect Login", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
 }
+
