606a86bbd4defdb1386ecad089811e900af5bb7b
kishnr <kishrmsh@gmail.com>
2020-03-18 15:19:50 +0530

    Menu and Login and Recipe

--- app/src/main/java/com/example/dashboard/Menu.java
+++ app/src/main/java/com/example/dashboard/Menu.java
@@ -2,13 +2,28 @@ package com.example.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    private Button view_recipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        view_recipe = (Button)findViewById(R.id.viewRecipe);
+
+        view_recipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Recipe.class);
+                startActivity(intent);
+            }
+        });
     }
 }
