20bbcec0a4e7744312e27ae1e2e5be7e89be46fa
username <kapilankk06@gmail.com>
2020-03-19 08:14:26 +0530

    update

--- app/src/main/java/com/example/dashboard/Recipe.java
+++ app/src/main/java/com/example/dashboard/Recipe.java
@@ -10,5 +10,6 @@ public class Recipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
     }
 }
