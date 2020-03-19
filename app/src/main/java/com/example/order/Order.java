6ecbdce7a967fe0891aefcfefce19de2aebb5f55
username <kapilankk06@gmail.com>
2020-03-19 08:19:40 +0530

    Order and User interface

--- app/src/main/java/com/example/order/Order.java
+++ app/src/main/java/com/example/order/Order.java
@@ -7,14 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Order extends AppCompatActivity {
 
     private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_order);
 
         button1=(Button) findViewById(R.id.button5);
         button1.setOnClickListener(new View.OnClickListener() {
