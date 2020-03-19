ab4936d657c2eaa20c78210d85ed385d10638165
kishnr <kishrmsh@gmail.com>
2020-03-19 07:53:45 +0530

    Menu and Login and Recipe

--- app/src/main/java/com/example/dashboard/Login.java
+++ app/src/main/java/com/example/dashboard/Login.java
@@ -18,7 +18,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
         name = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
