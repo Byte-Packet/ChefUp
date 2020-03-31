eebe78f4680a0679c31ec1139e932b62b79589de
Abishek <abishaj1998@gmail.com>
2020-03-31 19:22:10 +0530

    adding delivery

---
+++ app/src/main/java/com/example/dashboard/OrderConfirmed.java
@@ -0,0 +1,29 @@
+package com.example.dashboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class OrderConfirmed extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_confirmed);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(OrderConfirmed.this,Preparing.class);
+                startActivity(intent);
+            }
+        },5000);
+    }
+}
