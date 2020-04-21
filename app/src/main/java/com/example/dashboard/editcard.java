fb8182e879fe054f612a6bdfc8047cb9367a7c92
Abishek3344 <tathabi8@gmail.com>
2020-04-21 15:49:58 +0530

    view name,No,ed in editcard

--- app/src/main/java/com/example/dashboard/editcard.java
+++ app/src/main/java/com/example/dashboard/editcard.java
@@ -1,14 +1,39 @@
 package com.example.dashboard;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class editcard extends AppCompatActivity {
+    DatabaseHelper_pay myDb;
+
+    EditText edname,ednumber,eddate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editcard);
+
+        edname=(EditText)findViewById(R.id.editText);
+        ednumber=(EditText)findViewById(R.id.editText2);
+        eddate=(EditText)findViewById(R.id.editText7);
+
+        myDb = new DatabaseHelper_pay(this);
+        Cursor res =myDb.getAllData();
+
+        while(res.moveToNext()) {
+            edname.setText(res.getString(1));
+            ednumber.setText(res.getString(2));
+            eddate.setText(res.getString(3));
+
+
+
+
+        }
     }
 }
