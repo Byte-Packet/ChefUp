273379d009fdc1742a14b297fdfab60934071338
Abishek3344 <tathabi8@gmail.com>
2020-04-21 15:36:18 +0530

    View cNo,Ed in visa_card

--- app/src/main/java/com/example/dashboard/Visa_card.java
+++ app/src/main/java/com/example/dashboard/Visa_card.java
@@ -1,12 +1,16 @@
 package com.example.dashboard;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Visa_card extends AppCompatActivity {
+    DatabaseHelper_pay myDb;
+    TextView cn,ed;
 
     Intent i5;
     @Override
@@ -15,6 +19,19 @@ public class Visa_card extends AppCompatActivity {
         setContentView(R.layout.activity_visa_card);
 
         i5=new Intent(this,editcard.class);
+        myDb = new DatabaseHelper_pay(this);
+        cn=(TextView)findViewById(R.id.textView12);
+        ed=(TextView)findViewById(R.id.textView14);
+
+        Cursor res =myDb.getAllData();
+        while(res.moveToNext()) {
+
+            cn.setText(res.getString(2));
+            ed.setText(res.getString(3));
+
+
+        }
+
     }
 
     public void edit(View view) {
