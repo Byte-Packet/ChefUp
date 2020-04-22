78808fd147ee4d7b20878e5dbadb079adaf88b0c
kishnr <kishrmsh@gmail.com>
2020-04-22 14:37:21 +0530

    Trying to add Database Helper

--- app/src/main/java/com/example/dashboard/Recipe.java
+++ app/src/main/java/com/example/dashboard/Recipe.java
@@ -2,41 +2,36 @@ package com.example.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.example.dashboard.main.SectionsPagerAdapter;
 
 public class Recipe extends AppCompatActivity {
-    DatabaseHelper_reviews myDB;
+    DatabaseHelper myDB;
+    EditText name;
+    EditText review;
+    Button submit;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myDB = new DatabaseHelper_reviews(this);
+        myDB = new DatabaseHelper(this);
         setContentView(R.layout.activity_recipe);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+        name = (EditText)findViewById(R.id.review_Name);
+        review = (EditText)findViewById(R.id.review);
+        submit = (Button)findViewById(R.id.submit);
 
     }
 }
