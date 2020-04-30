95a0831fdbdecf51ef344522f2b68cb4d63a353f
Abishek <abishaj1998@gmail.com>
2020-04-30 23:09:11 +0530

    commit 30 april

--- app/src/main/java/com/example/dashboard/main/DeliveryData.java
+++ app/src/main/java/com/example/dashboard/main/DeliveryData.java
@@ -52,6 +52,7 @@ public class DeliveryData extends AppCompatActivity {
             }
         });
 
+        //add Button
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
